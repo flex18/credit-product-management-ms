f14c9a80311d686b16bbe05b9a9f302bbcf1ecc1
flex18 <emendozaminchan@gmail.com>
2024-03-01 17:13:07 -0500

    add credit product ms structure

---
+++ src/main/java/com/nttdata/credit/product/management/ms/CreditProductManagementMsApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.credit.product.management.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditProductManagementMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditProductManagementMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/credit/product/management/ms/CreditProductManagementMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.credit.product.management.ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditProductManagementMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
